d4da00e568d880800b23dbb7f47e427a6193a785
Ben <eastyinfinity1@gmail.com>
2026-03-02 15:04:23 +0000

    Removed Repeated Rooms + Added Max Distance

--- Room_Generator/Program.cs
+++ Room_Generator/Program.cs
@@ -7,27 +7,37 @@ class Program
         Random rnd = new Random();
         int roomType = 0;
         int deadEndChance = -10;
+        int maxDistance = 10;
         for (int i = 0; i <= distance; i++)
         {
             deadEndChance = deadEndChance + 5;
         }
         roomType = rnd.Next(1, roomTypes);
         int deadEnd = rnd.Next(1,100);
-        if (deadEnd < deadEndChance && deadEndChance > 0)
+        Console.WriteLine($"Dead End Number: {deadEnd}");
+        Console.WriteLine($"Dead End Chance: {deadEndChance}");
+        if (deadEnd < deadEndChance && deadEndChance > 0 || distance >= maxDistance)
         {
             roomType = 0;
+            Console.WriteLine("DeadEnd");
         }
         return(roomType);
+
     }
     static void Main(string[] args)
     {
-        int roomTypes = 10;
+        int roomTypes = 11; //+1 (11 = 10)
         int roomType = roomTypes + 1;
         int distance = 0;
+        int lastRoom = roomType;
         Console.WriteLine("Starter Room");
         while (roomType != 0)
         {
-            roomType = RandomNumber(distance, roomTypes);
+            while (roomType == lastRoom)
+            {
+                roomType = RandomNumber(distance, roomTypes);
+            }
+
             if (distance == 10)
             {
                 roomType = 0;
@@ -38,7 +48,9 @@ class Program
                 break;
             }
             distance++;
+
             Console.WriteLine($"Room Type: {roomType} Distance: {distance}");
+            lastRoom = roomType;
         }
         
     }
